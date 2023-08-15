a9e824d5f653711d7ca2e65e58649ff63ed4748b
Sam Mohr <sam@sammohr.dev>
2023-08-15 09:19:35 +0000

    Change wallpaper to work with any resolution

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -16,8 +16,8 @@
   ];
 
   home.file.".config/hypr/hyprpaper.conf".text = ''
-    preload = ${/. + ../../misc/wallpaper.jpg}
-    wallpaper = ,${/. + ../../misc/wallpaper.jpg}
+    preload = ${/. + ../../misc/rocket-launch.png}
+    wallpaper = ,${/. + ../../misc/rocket-launch.png}
     ipc = off
   '';
 }
