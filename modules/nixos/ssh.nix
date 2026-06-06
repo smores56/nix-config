25a12e9fda7ba89c6d3b6be54f9514687a8a85e4
Sam Mohr <sam@sammohr.dev>
2026-06-05 22:45:49 -0400

    fix(smortress): enable openssh + NOPASSWD sudo for headless access

--- modules/nixos/ssh.nix
+++ modules/nixos/ssh.nix
@@ -1,6 +1,8 @@
 { config, lib, ... }:
 {
   config = lib.mkIf config.dotfiles.exposeSsh {
+    services.openssh.enable = true;
+    security.sudo.wheelNeedsPassword = false;
     nix.settings.trusted-users = [ config.dotfiles.username ];
   };
 }
