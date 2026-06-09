1b4174ef19ec91f6f57b7e08398e0fdf4eeb648e
Sam Mohr <sam@sammohr.dev>
2026-06-09 08:47:10 -0400

    fix: pin nixpkgs, home-manager, noctalia, stylix to working revisions

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/f83fc3c307e74bc5fd5adb7eb6b8b13ffd2a36e1";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/57d5560ee92a424fb71fde800acd6ed2c725dfce";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # TODO: switch back to github:sodiboo/niri-flake when extraConfig or recent-windows is supported
     niri.url = "github:cmm/niri-flake/add-extraConfig";
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+      url = "github:noctalia-dev/noctalia-shell/2c1808f9f8937fc0b82c54af513f7620fec56d71";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     paneru = {
@@ -20,7 +20,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
-      url = "github:nix-community/stylix";
+      url = "github:nix-community/stylix/84971726c7ef0bb3669a5443e151cc226e65c518";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-parts.url = "github:hercules-ci/flake-parts";
